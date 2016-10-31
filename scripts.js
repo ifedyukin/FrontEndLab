bef345a3bca1bd4e56c292927e5649060f8237f0
Igor Fedyukin <isfedyukin@gmail.com>
2016-10-31 09:37:17 +0300

    Фиксануть названия и скобки

--- scripts.js
+++ scripts.js
@@ -1,37 +1,47 @@
-function li_1 (){
+function li_1() {
 	console.warn('%c[Задание 1]\n', 'background: black; color: white');
 	var a = "1 variable";
 	var b = "2 variable";
-	console.info(a +" and "+ b);
+	console.info(a + " and " + b);
 }
 
-function li_2 (a, h){
+function li_2(a, h) {
 	console.warn('%c[Задание 2]\n', 'background: black; color: white');
 	var s = a * h / 2;
 	console.info("Площадь треугольника с основанием " + a + " и высотой " + h + " равная " + s);
+	return s;
 }
 
-function li_3 (){
+function li_3() {
 	console.warn('%c[Задание 3]\n', 'background: black; color: white');
+
 	var arr = [5, 4, 3, 2, 1];
 	var work = 0;
+
 	console.info("Исходный массив: ");
-	for (i = 0; i <= 4; i++)
+
+	for (i = 0; i <= 4; i++) {
 		console.log(arr[i]);
+	}
+
 	console.info("Инвертируем массив через for: ");
-	for (i = 0; i <= 2; i++){
-		work = arr[4-i];
-		arr[4-i] = arr[i];
+
+	for (i = 0; i <= 2; i++) {
+		work = arr[4 - i];
+		arr[4 - i] = arr[i];
 		arr[i] = work;
 	}
-	for (i = 0; i <= 4; i++)
+
+	for (i = 0; i <= 4; i++) {
 		console.log(arr[i]);
+	}
+
 	console.info("Инвертируем массив через while: ");
+	
 	i = 0;
-	while(i <= 2)
-	{
-		work = arr[4-i];
-		arr[4-i] = arr[i];
+	while (i <= 2) {
+		work = arr[4 - i];
+		arr[4 - i] = arr[i];
 		arr[i] = work;
 		i++;
 	}
@@ -39,31 +49,30 @@ function li_3 (){
 		console.log(arr[i]);
 	console.info("Инвертируем массив через do-while: ");
 	i = 0;
-	do
-	{
-		work = arr[4-i];
-		arr[4-i] = arr[i];
+	do {
+		work = arr[4 - i];
+		arr[4 - i] = arr[i];
 		arr[i] = work;
 		i++;
-	} while(i <= 2)
+	} while (i <= 2)
 	for (i = 0; i <= 4; i++)
 		console.log(arr[i]);
 }
 
-function li_4(){
+function li_4() {
 	console.warn('%c[Задание 4]\n', 'background: black; color: white');
 	console.info("i++");
-	i=0;
-	while(i < 5)
+	var i = 0;
+	while (i < 5)
 		console.log(i++);
 	console.info("++i");
-	i=0;
-	while(i < 5)
+	i = 0;
+	while (i < 5)
 		console.log(++i);
 }
 
-function li_5(arg){
-	console.warn('%c[Задание 5]\n', 'background: black; color: white');	
+function li_5(arg) {
+	console.warn('%c[Задание 5]\n', 'background: black; color: white');
 	if (arg == 0)
 		console.info("Аргумент функции равен нулю");
 	else if (arg < 0)
@@ -72,14 +81,14 @@ function li_5(arg){
 		console.info("Аргумент функции положительный");
 }
 
-function li_6(){
-	console.warn('%c[Задание 6]\n', 'background: black; color: white');	
+function li_6() {
+	console.warn('%c[Задание 6]\n', 'background: black; color: white');
 	var name = prompt('Как тебя зовут?', '');
 	alert('Привет, ' + name);
 	console.info("Введено имя: " + name);
 }
 
-function li_7(num){
+function li_7(num) {
 	console.warn('%c[Задание 7]\n', 'background: black; color: white');
 	k = 1;
 	for (i = 1; i <= num; i++)
