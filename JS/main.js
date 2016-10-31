93e2962dda21b70d5f6d86cc5e86adb497687357
Igor Fedyukin <isfedyukin@gmail.com>
2016-10-31 10:04:36 +0300

    Fixed

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,20 @@
-function li_1() {
+function printVariables() {
 	console.warn('%c[Задание 1]\n', 'background: black; color: white');
 	var a = "1 variable";
 	var b = "2 variable";
-	console.info(a + " and " + b);
+	var reply = a + " and " + b;
+	console.info(reply);
+	return reply;
 }
 
-function li_2(a, h) {
+function sTriangle(a, h) {
 	console.warn('%c[Задание 2]\n', 'background: black; color: white');
 	var s = a * h / 2;
 	console.info("Площадь треугольника с основанием " + a + " и высотой " + h + " равная " + s);
 	return s;
 }
 
-function li_3() {
+function invert() {
 	console.warn('%c[Задание 3]\n', 'background: black; color: white');
 
 	var arr = [5, 4, 3, 2, 1];
@@ -37,7 +39,7 @@ function li_3() {
 	}
 
 	console.info("Инвертируем массив через while: ");
-	
+
 	i = 0;
 	while (i <= 2) {
 		work = arr[4 - i];
@@ -55,43 +57,61 @@ function li_3() {
 		arr[i] = work;
 		i++;
 	} while (i <= 2)
-	for (i = 0; i <= 4; i++)
+	for (i = 0; i <= 4; i++) {
 		console.log(arr[i]);
+	}
+	return arr;
 }
 
-function li_4() {
+function prepost() {
 	console.warn('%c[Задание 4]\n', 'background: black; color: white');
 	console.info("i++");
+	var reply = [];
 	var i = 0;
-	while (i < 5)
-		console.log(i++);
+	while (i < 5) {
+		console.log(i++)
+	};
 	console.info("++i");
-	i = 0;
-	while (i < 5)
-		console.log(++i);
+	var j = 0;
+	while (j < 5) {
+		console.log(++j)
+	};
+	reply.push(i);
+	reply.push(j);
+	return reply;
 }
 
-function li_5(arg) {
+function argValue(arg) {
 	console.warn('%c[Задание 5]\n', 'background: black; color: white');
-	if (arg == 0)
-		console.info("Аргумент функции равен нулю");
-	else if (arg < 0)
-		console.info("Аргумент функции отрицательный");
-	else if (arg > 0)
-		console.info("Аргумент функции положительный");
+	var reply;
+	if (arg == 0) {
+		reply = "Аргумент функции равен нулю";
+	}
+	else if (arg < 0) {
+		reply = "Аргумент функции отрицательный";
+	}
+	else if (arg > 0) {
+		reply = "Аргумент функции положительный";
+	}
+	else {
+		reply = "Ошибка!";
+	}
+	return reply;
 }
 
-function li_6() {
+function whatsName() {
 	console.warn('%c[Задание 6]\n', 'background: black; color: white');
 	var name = prompt('Как тебя зовут?', '');
 	alert('Привет, ' + name);
 	console.info("Введено имя: " + name);
+	return name;
 }
 
-function li_7(num) {
+function factorial(num) {
 	console.warn('%c[Задание 7]\n', 'background: black; color: white');
 	k = 1;
 	for (i = 1; i <= num; i++)
 		k *= i;
 	console.info("Факториал " + num + " равен " + k);
+	return k;
 }
