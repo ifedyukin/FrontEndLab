a6495dcedca8c96c6a39e34aceb3499c11bb95fb
Igor Fedyukin <isfedyukin@gmail.com>
2016-11-01 14:37:17 +0300

    Fix

--- Task1/JS/main.js
+++ Task1/JS/main.js
@@ -1,36 +1,18 @@
 function largestInt(a, b) {
-	console.warn('%c[Задание 1]\n', 'background: black; color: white');
-	console.info("Введены числа: " + a + " и " + b);
-	return a >= b  ? q : b;
+	return a >= b  ? a : b;
 }
 
 function odd(number) {
-	var reply;
-	if (number % 2 > 0) {
-		reply = 'нечетное';
-	}
-	else {
-		reply = 'четное';
-	}
-	return reply;
+	return (number % 2 > 0) ? false : true;
 }
 
 function forOdd() {
-	console.warn('%c[Задание 2]\n', 'background: black; color: white');
 	for (i = 0; i <= 9; i++) {
 		console.log(i + ' - ' + odd(i));
 	}
 }
 
 function is_Blank(string) {
-	console.info("Введена строка: '" + string + "'");
 	string = string.replace(/\s{2,}/g, ' ');
-	if (string != ' ' && string != '') {
-		result = true;
-	}
-	else {
-		result = false;
-	}
-	console.log(result);
-	return result;
+	return (string != ' ' && string != '') ? true : false;
 }
--- Task2/JS/main.js
+++ Task2/JS/main.js
@@ -3,10 +3,8 @@
 
 
 	function isPalindrome(string) {
-		console.warn('%c[Задание 1]\n', 'background: black; color: white');
-		console.info('Введена строка "' + string + '"');
 		var result = true;
-		for (i = 1; i <= string.length; i++) {
+		for (var i = 1; i <= string.length; i++) {
 			if (string[i - 1] != string[string.length - i]) {
 				result = false;
 			}
@@ -15,16 +13,13 @@
 		return result;
 	}
 
-	console.log(isPalindrome('fdfdf') ? console.log('Строка - полиндром') : console.log('Строка - не полиндром'));
+	console.log(isPalindrome('lalal') ? console.log('Строка - полиндром') : console.log('Строка - не полиндром'));
 
 	function genCombinations(string) {
-		console.warn('%c[Задание 2]\n', 'background: black; color: white');
-		console.info('Введена строка "' + string + '"');
-
 		var reply = [];
 
-		for (i = 0; i <= string.length; i++) {
-			for (j = i; j <= string.length; j++) {
+		for (var i = 0; i <= string.length; i++) {
+			for (var j = i; j <= string.length; j++) {
 				if (j > i) {
 					reply.push(string.substring(i, j));
 				}
@@ -34,9 +29,9 @@
 		return reply;
 	}
 
+	console.log(genCombinations("dog"));
+
 	function sort(string) {
-		console.warn('%c[Задание 3]\n', 'background: black; color: white');
-		console.info('Введена строка "' + string + '"');
 		string = string.replace(/\s/g, '');
 		var m = string.split('');
 		for (var i = 0; i < 9; i++) {
@@ -50,14 +45,12 @@
 				}
 			}
 		}
-		console.info("Строка отсортирована");
-		console.log(m);
 		return m;
 	}
 
+	console.log(sort("lorem ipsum dolor"));
+
 	function longestWord(string) {
-		console.warn('%c[Задание 4]\n', 'background: black; color: white');
-		console.info('Введена строка "' + string + '"');
 		string = string.split(' ');
 		var max = string[0];
 		for (var i = 1; i < string.length; i++) {
@@ -68,19 +61,21 @@
 		return max;
 	}
 
+	console.log(longestWord("lorem lor em"));
+
 	function extractUnique(string) {
-		console.warn('%c[Задание 5]\n', 'background: black; color: white');
-		console.info('Введена строка "' + string + '"');
 		var str = string.split('');
-		for (i = 0; i < str.length; i++) {
-			for (j = 0; j < str.length; j++) {
+		for (var i = 0; i < str.length; i++) {
+			for (var j = 0; j < str.length; j++) {
 				if (str[i] == str[j] && i != j) {
 					str[j] = "";
 				}
 			}
 		}
 		str = str.toString().replace(/,/g, "");
-		console.log(str);
 		return str;
 	}
+
+	console.log(extractUnique("rreqrwerfdsfefrwefdfweerew"));
+
 } ());
