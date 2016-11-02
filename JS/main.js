c294fb0eacc40da3da16abc7ac36bb3e1930cb1f
Igor Fedyukin <isfedyukin@gmail.com>
2016-11-02 11:48:12 +0300

    Fix

--- JS/main.js
+++ JS/main.js
@@ -12,63 +12,68 @@ var Utils = (function () {
 } ());
 
 //Модуль с калькулятором
-(function (Utils) {
+var Calculator = (function (Utils) {
   'use strict';
 
   var result = 0;
 
-  var calculator = {
-
-    add: function add(value) {
-      if (Utils.isNumber(value)) {
-        result += value;
-        return add;
-      } else {
-        throw new Error('Function argument isn\'t number!');
-      }
-    },
-
-    subtract: function subtract(value) {
-      if (Utils.isNumber(value)) {
-        result -= value;
-        return subtract;
-      } else {
-        throw new Error('Function argument isn\'t number!');
-      }
-    },
-
-    divide: function divide(value) {
-      if (Utils.isNumber(value)) {
-        result /= value;
-        return divide;
-      } else {
-        throw new Error('Function argument isn\'t number!');
-      }
-    },
-
-    multiply: function multiply(value) {
-      if (Utils.isNumber(value)) {
-        result *= value;
-        return multiply;
-      } else {
-        throw new Error('Function argument isn\'t number!');
-      }
-    },
-
-    getResult: function () {
-      return result;
-    },
-
-    reset: function () {
-      return result = 0;
+  function add(value) {
+    if (Utils.isNumber(value)) {
+      result += value;
+      return add;
+    } else {
+      throw new Error('Function argument isn\'t number!');
     }
-  };
+  }
 
+  function subtract(value) {
+    if (Utils.isNumber(value)) {
+      result -= value;
+      return subtract;
+    } else {
+      throw new Error('Function argument isn\'t number!');
+    }
+  }
 
-  //Вычисления
-  calculator.add(3)(2)(1); //6
-  calculator.divide(2);    //3
-  calculator.subtract(1);  //2
-  console.log(calculator.getResult());
+  function divide(value) {
+    if (Utils.isNumber(value)) {
+      result /= value;
+      return divide;
+    } else {
+      throw new Error('Function argument isn\'t number!');
+    }
+  }
+
+  function multiply(value) {
+    if (Utils.isNumber(value)) {
+      result *= value;
+      return multiply;
+    } else {
+      throw new Error('Function argument isn\'t number!');
+    }
+  }
+
+  function getResult() {
+    return result;
+  }
+
+  function reset() {
+    return result = 0;
+  }
+
+  return {
+    add: add,
+    subtract: subtract,
+    divide: divide,
+    multiply: multiply,
+    getResult: getResult,
+    reset: reset
+  }
 
 } (Utils));
+
+Calculator.add(3)(2)(1);  //6
+Calculator.subtract(2);   //4
+Calculator.divide(2);     //2
+Calculator.multiply(3);   //6
+console.log(Calculator.getResult());
